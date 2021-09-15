5356747fbd59f1def072a87c1f71957c4a9632e1
Dastrix2021 <80123370+Dastrix2021@users.noreply.github.com>
2021-09-15 23:16:02 +0300

    test

---
+++ src/com/dastrix/Main.java
@@ -0,0 +1,23 @@
+package com.dastrix;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] a;
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Количество элемнетов в массиве: ");
+        n = in.nextInt ();
+        a = new int [n];
+        int i = 0;
+        do {
+            System.out.print("Введите a[" + i + "] = ");
+            a[i] = in.nextInt ();
+            i++;
+        } while (i < n);
+
+    }
+}
